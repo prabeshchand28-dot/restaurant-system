1641ba2578d255c2b7cb67e4ccc705b67b6a5eeb
Prabesh Chand <prabeshchand143@gmail.com>
2026-06-15 16:15:44 +0900

    feat: merge QR ordering into Tables section

--- controllers/tableController.js
+++ controllers/tableController.js
@@ -1,7 +1,7 @@
 // controllers/tableController.js
 const prisma  = require('../config/prisma');
 const { generateQRDataUrl } = require('../utils/qrGenerator');
-const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
+const BASE_URL = process.env.PUBLIC_URL || process.env.BASE_URL || 'http://localhost:3000';
 
 exports.getAll = async (req, res) => {
   try {
@@ -34,7 +34,7 @@ exports.remove = async (req, res) => {
 exports.getQR = async (req, res) => {
   try {
     const no  = parseInt(req.params.no);
-    const url = `${BASE_URL}/order?table=${no}`;
+    const url = `${BASE_URL}/menu?table=${no}`;
     const dataUrl = await generateQRDataUrl(url);
     res.json({ success: true, dataUrl, url });
   } catch (e) { res.status(500).json({ success: false, message: e.message }); }
